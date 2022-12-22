ea892624ebfdcabf162ed5e7f848e65536caf9eb
Mark Weisgerber <m.weisgerber@datamints.com>
2022-12-22 16:49:04 +0100

    dev

---
+++ Configuration/TCA/Overrides/tx_staticcitiesde_domain_model_record.php
@@ -0,0 +1,4 @@
+<?php
+
+## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
+$GLOBALS['TCA']['tx_staticcitiesde_domain_model_record']['ctrl']['iconfile'] = 'EXT:static_cities_de/Resources/Public/Icons/tx_staticcitiesde_domain_model_record.png';
