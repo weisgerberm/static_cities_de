bc4b9e20dc680cd65cab966307b040cedb6a7645
Mark Weisgerber <m.weisgerber@datamints.com>
2024-10-16 00:03:50 +0200

    migrations for TYPO3 13

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,5 @@
 defined('TYPO3') || die();
 
 (static function() {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_staticcitiesde_domain_model_record', 'EXT:static_cities_de/Resources/Private/Language/locallang_csh_tx_staticcitiesde_domain_model_record.xlf');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_staticcitiesde_domain_model_record');
+
 })();
